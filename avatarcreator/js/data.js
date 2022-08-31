d1e10b594d50186c1096aa076cbccbe5aed3cb3b
chomapublicize <choma@publicize.co>
2022-08-31 08:55:43 -0500

    starter

---
+++ avatarcreator/js/data.js
@@ -0,0 +1,72 @@
+[
+    {
+      "hair": [
+        "hair-style-01", 
+        "hair-style-02",
+        "hair-style-04",
+        "hair-style-05"
+      ],
+      "accessories": [
+        "accessories-style-01", 
+        "accessories-style-02"
+      ],
+      "bg": [
+        "bg-style-01", 
+        "bg-style-02",
+        "bg-style-03", 
+        "bg-style-04"
+      ],
+      "eyes": [
+        "eyes-style-01",
+        "eyes-style-02",
+        "eyes-style-03", 
+        "eyes-style-04",
+        "eyes-style-05" 
+      ],
+      "face": [
+        "face-style-01", 
+        "face-style-02",
+        "face-style-03", 
+        "face-style-04",
+        "face-style-05"
+      ],
+      "mouth": [
+        "mouth-style-01", 
+        "mouth-style-02",
+        "mouth-style-03", 
+        "mouth-style-04",
+        "mouth-style-05", 
+        "mouth-style-06",
+        "mouth-style-07", 
+        "mouth-style-08",
+        "mouth-style-09",
+        "mouth-style-10"
+      ],
+      "outfit": [
+        "outfit-style-01", 
+        "outfit-style-02",
+        "outfit-style-03",
+        "outfit-style-04",
+        "outfit-style-05",
+        "outfit-style-06",
+        "outfit-style-07",
+        "outfit-style-08",
+        "outfit-style-09",
+        "outfit-style-10",
+        "outfit-style-11",
+        "outfit-style-12",
+        "outfit-style-13",
+        "outfit-style-14",
+        "outfit-style-15",
+        "outfit-style-16",
+        "outfit-style-17",
+        "outfit-style-18",
+        "outfit-style-19",
+        "outfit-style-20",
+        "outfit-style-21",
+        "outfit-style-22",
+        "outfit-style-23"
+      ]
+    }
+  ]
+  
