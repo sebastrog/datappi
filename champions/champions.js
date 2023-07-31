cfa155f5eb6dfa1067cd7003b36e197e5facc755
chomapublicize <choma@publicize.co>
2023-07-31 07:12:11 -0500

    add lol champions

---
+++ champions/champions.js
@@ -0,0 +1,130 @@
+export default champions = [
+  {
+    "publish_details": {
+      "locale": "en-us"
+    },
+    "uid": "bltf1c826326a26cd46",
+    "url": "/champions/aatrox/",
+    "data_dragon_id": "Aatrox",
+    "data_dragon_json": "{\"id\":\"Aatrox\",\"key\":\"266\",\"name\":\"Aatrox\",\"title\":\"the Darkin Blade\",\"image\":{\"full\":\"Aatrox.png\",\"sprite\":\"champion0.png\",\"group\":\"champion\",\"x\":0,\"y\":0,\"w\":48,\"h\":48},\"skins\":[{\"id\":\"266000\",\"num\":0,\"name\":\"default\",\"chromas\":false},{\"id\":\"266001\",\"num\":1,\"name\":\"Justicar Aatrox\",\"chromas\":false},{\"id\":\"266002\",\"num\":2,\"name\":\"Mecha Aatrox\",\"chromas\":true},{\"id\":\"266003\",\"num\":3,\"name\":\"Sea Hunter Aatrox\",\"chromas\":false},{\"id\":\"266007\",\"num\":7,\"name\":\"Blood Moon Aatrox\",\"chromas\":false},{\"id\":\"266008\",\"num\":8,\"name\":\"Prestige Blood Moon Aatrox\",\"chromas\":false},{\"id\":\"266009\",\"num\":9,\"name\":\"Victorious Aatrox\",\"chromas\":true},{\"id\":\"266011\",\"num\":11,\"name\":\"Odyssey Aatrox\",\"chromas\":true},{\"id\":\"266020\",\"num\":20,\"name\":\"Prestige Blood Moon Aatrox (2022)\",\"chromas\":false},{\"id\":\"266021\",\"num\":21,\"name\":\"Lunar Eclipse Aatrox\",\"chromas\":true},{\"id\":\"266030\",\"num\":30,\"name\":\"DRX Aatrox\",\"chromas\":true},{\"id\":\"266031\",\"num\":31,\"name\":\"Prestige DRX Aatrox\",\"chromas\":false}],\"lore\":\"Once honored defenders of Shurima against the Void, Aatrox and his brethren would eventually become an even greater threat to Runeterra, and were defeated only by cunning mortal sorcery. But after centuries of imprisonment, Aatrox was the first to find freedom once more, corrupting and transforming those foolish enough to try and wield the magical weapon that contained his essence. Now, with stolen flesh, he walks Runeterra in a brutal approximation of his previous form, seeking an apocalyptic and long overdue vengeance.\",\"blurb\":\"Once honored defenders of Shurima against the Void, Aatrox and his brethren would eventually become an even greater threat to Runeterra, and were defeated only by cunning mortal sorcery. But after centuries of imprisonment, Aatrox was the first to find...\",\"allytips\":[\"Use Umbral Dash while casting The Darkin Blade to increase your chances of hitting the enemy.\",\"Crowd Control abilities like Infernal Chains or your allies' immobilizing effects will help you set up The Darkin Blade.\",\"Cast World Ender when you are sure you can force a fight.\"],\"enemytips\":[\"Aatrox's attacks are very telegraphed, so use the time to dodge the hit zones.\",\"Aatrox's Infernal Chains are easier to exit when running towards the sides or at Aatrox.\",\"Keep your distance when Aatrox uses his Ultimate to prevent him from reviving.\"],\"tags\":[\"Fighter\",\"Tank\"],\"partype\":\"Blood Well\",\"info\":{\"attack\":8,\"defense\":4,\"magic\":3,\"difficulty\":4},\"stats\":{\"hp\":650,\"hpperlevel\":114,\"mp\":0,\"mpperlevel\":0,\"movespeed\":345,\"armor\":38,\"armorperlevel\":4.45,\"spellblock\":32,\"spellblockperlevel\":2.05,\"attackrange\":175,\"hpregen\":3,\"hpregenperlevel\":1,\"mpregen\":0,\"mpregenperlevel\":0,\"crit\":0,\"critperlevel\":0,\"attackdamage\":60,\"attackdamageperlevel\":5,\"attackspeedperlevel\":2.5,\"attackspeed\":0.651},\"spells\":[{\"id\":\"AatroxQ\",\"name\":\"The Darkin Blade\",\"description\":\"Aatrox slams his greatsword down, dealing physical damage. He can swing three times, each with a different area of effect.\",\"tooltip\":\"Aatrox slams his greatsword, dealing <physicalDamage>{{ qdamage }} physical damage</physicalDamage>. If they are hit on the edge, they are briefly <status>Knocked Up</status> and they take <physicalDamage>{{ qedgedamage }}</physicalDamage> instead. This Ability can be <recast>Recast</recast> twice, each one changing shape and dealing 25% more damage than the previous one.{{ spellmodifierdescriptionappend }}\",\"leveltip\":{\"label\":[\"Cooldown\",\"Damage\",\"Total AD Ratio\"],\"effect\":[\"{{ cooldown }} -> {{ cooldownNL }}\",\"{{ qbasedamage }} -> {{ qbasedamageNL }}\",\"{{ qtotaladratio*100.000000 }}% -> {{ qtotaladrationl*100.000000 }}%\"]},\"maxrank\":5,\"cooldown\":[14,12,10,8,6],\"cooldownBurn\":\"14/12/10/8/6\",\"cost\":[0,0,0,0,0],\"costBurn\":\"0\",\"datavalues\":{},\"effect\":[null,[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],\"effectBurn\":[null,\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\"],\"vars\":[],\"costType\":\"No Cost\",\"maxammo\":\"-1\",\"range\":[25000,25000,25000,25000,25000],\"rangeBurn\":\"25000\",\"image\":{\"full\":\"AatroxQ.png\",\"sprite\":\"spell0.png\",\"group\":\"spell\",\"x\":384,\"y\":48,\"w\":48,\"h\":48},\"resource\":\"No Cost\"},{\"id\":\"AatroxW\",\"name\":\"Infernal Chains\",\"description\":\"Aatrox smashes the ground, dealing damage to the first enemy hit. Champions and large monsters have to leave the impact area quickly or they will be dragged to the center and take the damage again.\",\"tooltip\":\"Aatrox fires a chain, <status>Slowing</status> the first enemy hit by {{ wslowpercentage*-100 }}% for {{ wslowduration }} seconds and dealing <physicalDamage>{{ wdamage }} physical damage</physicalDamage>. Champions and large jungle monsters have {{ wslowduration }} seconds to leave the impact area or be <status>Pulled</status> back to the center and damaged again for the same amount.{{ spellmodifierdescriptionappend }}\",\"leveltip\":{\"label\":[\"Cooldown\",\"Damage\",\"Slow\"],\"effect\":[\"{{ cooldown }} -> {{ cooldownNL }}\",\"{{ wbasedamage }} -> {{ wbasedamageNL }}\",\"{{ wslowpercentage*-100.000000 }}% -> {{ wslowpercentagenl*-100.000000 }}%\"]},\"maxrank\":5,\"cooldown\":[20,18,16,14,12],\"cooldownBurn\":\"20/18/16/14/12\",\"cost\":[0,0,0,0,0],\"costBurn\":\"0\",\"datavalues\":{},\"effect\":[null,[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],\"effectBurn\":[null,\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\"],\"vars\":[],\"costType\":\"No Cost\",\"maxammo\":\"-1\",\"range\":[825,825,825,825,825],\"rangeBurn\":\"825\",\"image\":{\"full\":\"AatroxW.png\",\"sprite\":\"spell0.png\",\"group\":\"spell\",\"x\":432,\"y\":48,\"w\":48,\"h\":48},\"resource\":\"No Cost\"},{\"id\":\"AatroxE\",\"name\":\"Umbral Dash\",\"description\":\"Passively, Aatrox heals when damaging enemy champions. On activation, he dashes in a direction.\",\"tooltip\":\"<spellPassive>Passive:</spellPassive> Aatrox gains <lifeSteal>{{ espellvamp }}% Omnivamp</lifeSteal> against champions, increased to <lifeSteal>{{ espellvampempowered }}% Omnivamp</lifeSteal> during <keywordMajor>World Ender</keywordMajor>.<br /><br /><spellActive>Active:</spellActive> Aatrox dashes. He can use this Ability while winding up his other Abilities.{{ spellmodifierdescriptionappend }}\",\"leveltip\":{\"label\":[\"Cooldown\",\"Healing %\",\"Healing % during World Ender\"],\"effect\":[\"{{ cooldown }} -> {{ cooldownNL }}\",\"{{ espellvamp }}% -> {{ espellvampNL }}%\",\"{{ espellvampempowered }}% -> {{ espellvampempoweredNL }}%\"]},\"maxrank\":5,\"cooldown\":[9,8,7,6,5],\"cooldownBurn\":\"9/8/7/6/5\",\"cost\":[0,0,0,0,0],\"costBurn\":\"0\",\"datavalues\":{},\"effect\":[null,[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],\"effectBurn\":[null,\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\"],\"vars\":[],\"costType\":\"No Cost\",\"maxammo\":\"1\",\"range\":[25000,25000,25000,25000,25000],\"rangeBurn\":\"25000\",\"image\":{\"full\":\"AatroxE.png\",\"sprite\":\"spell0.png\",\"group\":\"spell\",\"x\":0,\"y\":96,\"w\":48,\"h\":48},\"resource\":\"No Cost\"},{\"id\":\"AatroxR\",\"name\":\"World Ender\",\"description\":\"Aatrox unleashes his demonic form, fearing nearby enemy minions and gaining attack damage, increased healing, and Move Speed. If he gets a takedown, this effect is extended.\",\"tooltip\":\"Aatrox reveals his true demonic form, <status>Fearing</status> nearby minions for {{ rminionfearduration }} seconds and gaining <speed>{{ rmovementspeedbonus*100 }}% Move Speed</speed> decaying over {{ rduration }} seconds. He also gains <scaleAD>{{ rtotaladamp*100 }}% Attack Damage</scaleAD> and increases <healing>self-healing by {{ rhealingamp*100 }}%</healing> for the duration.<br /><br />Champion takedowns extend the duration of this effect by {{ rextension }} seconds and refresh the <speed>Move Speed</speed> effect.{{ spellmodifierdescriptionappend }}\",\"leveltip\":{\"label\":[\"Total Attack Damage Increase\",\"Healing Increase\",\"Move Speed\",\"Cooldown\"],\"effect\":[\"{{ rtotaladamp*100.000000 }}% -> {{ rtotaladampnl*100.000000 }}%\",\"{{ rhealingamp*100.000000 }}% -> {{ rhealingampnl*100.000000 }}%\",\"{{ rmovementspeedbonus*100.000000 }}% -> {{ rmovementspeedbonusnl*100.000000 }}%\",\"{{ cooldown }} -> {{ cooldownNL }}\"]},\"maxrank\":3,\"cooldown\":[120,100,80],\"cooldownBurn\":\"120/100/80\",\"cost\":[0,0,0],\"costBurn\":\"0\",\"datavalues\":{},\"effect\":[null,[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]],\"effectBurn\":[null,\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\",\"0\"],\"vars\":[],\"costType\":\"No Cost\",\"maxammo\":\"-1\",\"range\":[25000,25000,25000],\"rangeBurn\":\"25000\",\"image\":{\"full\":\"AatroxR.png\",\"sprite\":\"spell0.png\",\"group\":\"spell\",\"x\":48,\"y\":96,\"w\":48,\"h\":48},\"resource\":\"No Cost\"}],\"passive\":{\"name\":\"Deathbringer Stance\",\"description\":\"Periodically, Aatrox's next basic attack deals bonus <physicalDamage>physical damage</physicalDamage> and heals him, based on the target's max health. \",\"image\":{\"full\":\"Aatrox_Passive.png\",\"sprite\":\"passive0.png\",\"group\":\"passive\",\"x\":0,\"y\":0,\"w\":48,\"h\":48}},\"recommended\":[]}",
+    "champion_blurb": "Once honored defenders of Shurima against the Void, Aatrox and his brethren would eventually become an even greater threat to Runeterra, and were defeated only by cunning mortal sorcery. But after centuries of imprisonment, Aatrox was the first to find...",
+    "lore": "Once honored defenders of Shurima against the Void, Aatrox and his brethren would eventually become an even greater threat to Runeterra, and were defeated only by cunning mortal sorcery. But after centuries of imprisonment, Aatrox was the first to find freedom once more, corrupting and transforming those foolish enough to try and wield the magical weapon that contained his essence. Now, with stolen flesh, he walks Runeterra in a brutal approximation of his previous form, seeking an apocalyptic and long overdue vengeance.",
+    "champion_icon": "https://ddragon.leagueoflegends.com/cdn/13.14.1/img/champion/Aatrox.png",
+    "champion_name": "Aatrox",
+    "champion_splash": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+    "champion_title": "the Darkin Blade",
+    "recommended_lanes": [],
+    "recommended_roles": [
+      "Fighter",
+      "Tank"
+    ],
+    "links": [
+      {
+        "href": "https://u.gg/lol/champions/aatrox/build",
+        "title": "U.gg"
+      },
+      {
+        "href": "https://na.op.gg/champion/Aatrox/statistics/mid",
+        "title": "Op.gg"
+      },
+      {
+        "href": "https://www.probuilds.net/champions/details/Aatrox",
+        "title": "probuilds.net"
+      }
+    ],
+    "champion": {
+      "modular_blocks": []
+    },
+    "skins": [
+      {
+        "name": "default",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg"
+      },
+      {
+        "name": "Justicar Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_1.jpg"
+      },
+      {
+        "name": "Mecha Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_2.jpg"
+      },
+      {
+        "name": "Sea Hunter Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_3.jpg"
+      },
+      {
+        "name": "Blood Moon Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_7.jpg"
+      },
+      {
+        "name": "Prestige Blood Moon Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_8.jpg"
+      },
+      {
+        "name": "Victorious Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_9.jpg"
+      },
+      {
+        "name": "Odyssey Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_11.jpg"
+      },
+      {
+        "name": "Prestige Blood Moon Aatrox (2022)",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_20.jpg"
+      },
+      {
+        "name": "Lunar Eclipse Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_21.jpg"
+      },
+      {
+        "name": "DRX Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_30.jpg"
+      },
+      {
+        "name": "Prestige DRX Aatrox",
+        "imageUrl": "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_31.jpg"
+      }
+    ],
+    "champion_passive": {
+      "champion_passive_description": "Periodically, Aatrox's next basic attack deals bonus physical damage and heals him, based on the target's max health. ",
+      "champion_passive_icon": "https://ddragon.leagueoflegends.com/cdn/13.14.1/img/passive/Aatrox_Passive.png",
+      "champion_passive_name": "Deathbringer Stance",
+      "champion_passive_video_mp4": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_P1.mp4",
+      "champion_passive_video_poster": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_P1.jpg",
+      "champion_passive_video_webm": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_P1.webm"
+    },
+    "champion_q": {
+      "champion_q_description": "Aatrox slams his greatsword down, dealing physical damage. He can swing three times, each with a different area of effect.",
+      "champion_q_icon": "https://ddragon.leagueoflegends.com/cdn/13.14.1/img/spell/AatroxQ.png",
+      "champion_q_name": "The Darkin Blade",
+      "champion_q_video_mp4": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_Q1.mp4",
+      "champion_q_video_poster": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_Q1.jpg",
+      "champion_q_video_webm": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_Q1.webm"
+    },
+    "champion_w": {
+      "champion_w_description": "Aatrox smashes the ground, dealing damage to the first enemy hit. Champions and large monsters have to leave the impact area quickly or they will be dragged to the center and take the damage again.",
+      "champion_w_icon": "https://ddragon.leagueoflegends.com/cdn/13.14.1/img/spell/AatroxW.png",
+      "champion_w_name": "Infernal Chains",
+      "champion_w_video_mp4": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_W1.mp4",
+      "champion_w_video_poster": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_W1.jpg",
+      "champion_w_video_webm": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_W1.webm"
+    },
+    "champion_e": {
+      "champion_e_description": "Passively, Aatrox heals when damaging enemy champions. On activation, he dashes in a direction.",
+      "champion_e_icon": "https://ddragon.leagueoflegends.com/cdn/13.14.1/img/spell/AatroxE.png",
+      "champion_e_name": "Umbral Dash",
+      "champion_e_video_mp4": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_E1.mp4",
+      "champion_e_video_poster": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_E1.jpg",
+      "champion_e_video_webm": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_E1.webm"
+    },
+    "champion_r": {
+      "champion_r_description": "Aatrox unleashes his demonic form, fearing nearby enemy minions and gaining attack damage, increased healing, and Move Speed. If he gets a takedown, this effect is extended.",
+      "champion_r_icon": "https://ddragon.leagueoflegends.com/cdn/13.14.1/img/spell/AatroxR.png",
+      "champion_r_name": "World Ender",
+      "champion_r_video_mp4": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_R1.mp4",
+      "champion_r_video_poster": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_R1.jpg",
+      "champion_r_video_webm": "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0266/ability_0266_R1.webm"
+    },
+    "related_champions": []
+  }
+]
